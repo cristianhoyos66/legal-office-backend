5a37bc5b0be88a8ad8fe632c6038e0c47da83b06
cristianhoyos66 <cristianhoyos66@gmail.com>
2022-10-31 01:25:37 -0500

    Fix refresh

--- src/routes/login.route.ts
+++ src/routes/login.route.ts
@@ -14,7 +14,7 @@ router.post('/login', asyncHandler(async (req, res, next) => {
       sameSite: 'lax',
       maxAge: 1800000,
       path: '/api/refresh',
-      domain: 'seashell-app-coja7.ondigitalocean.app'
+      domain: 'consultorio-juridico.herokuapp.com'
     })
     res.send({ accessToken: response.accessToken })
   } catch (error) {
@@ -36,7 +36,7 @@ router.post('/refresh', asyncHandler(async (req, res, next) => {
       sameSite: 'lax',
       maxAge: 1800000,
       path: '/api/refresh',
-      domain: 'seashell-app-coja7.ondigitalocean.app'
+      domain: 'consultorio-juridico.herokuapp.com/'
     })
     res.send({ accessToken: response.accessToken })
   } catch (error) {
